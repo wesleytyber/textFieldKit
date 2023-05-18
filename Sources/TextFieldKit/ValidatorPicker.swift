8e2f2bfa6c0ae591596616144b193147eae50df3
wesley.santos <wesley.santos@brave.ag>
2023-05-18 18:37:39 -0300

    feat: [master] - Added components for picker view

---
+++ Sources/TextFieldKit/ValidatorPicker.swift
@@ -0,0 +1,89 @@
+//
+//  MetabolicRateView.swift
+//  CalorieTracker
+//
+//  Created by Wesley Calazans on 18/05/23.
+//
+
+import UIKit
+
+public class ValidatorInputPicker: ValidatorInput {
+    var rotateEnabled = true
+    var paddingEnabled = true
+    
+    var imageView: UIImageView = {
+        let imageView = UIImageView(image: Asset.Icons.arrowGreenDown.image)
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        let frame = CGRect(x: bounds.minX+16, y: bounds.minY+4,
+                           width: bounds.maxX-56, height: bounds.maxY-8)
+        return paddingEnabled ? frame : super.editingRect(forBounds: bounds)
+    }
+
+}
+
+private extension ValidatorInputPicker {
+    func setup() {
+        self.autocorrectionType = .no
+        self.inputEnabled = false
+        self.rightViewMode = .always        
+        
+        let v = UIView()
+        v.height(constant: 10)
+        v.width(constant: 56)
+        
+        v.addSubview(imageView)
+        imageView.size(constant: 12)
+        imageView.centerX(equalTo: v)
+        
+        rightView = v
+        
+        tintColor = .clear
+    }
+}
+
+public extension ValidatorInputPicker {
+    override func textFieldDidBeginEditing(_ textField: UITextField) {
+        super.textFieldDidBeginEditing(textField)
+        if self.rotateEnabled {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            })
+        }
+    }
+    
+    override func textFieldDidEndEditing(_ textField: UITextField) {
+        super.textFieldDidEndEditing(textField)
+        if self.rotateEnabled {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.imageView.transform = CGAffineTransform(rotationAngle: 0)
+            })
+        }
+    }
+    
+    override func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return false
+    }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        if action == #selector(paste(_:)) {
+            return false
+        }
+
+        return false
+    }
+}
